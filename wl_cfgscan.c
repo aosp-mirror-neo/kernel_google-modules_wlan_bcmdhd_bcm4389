3c27c64da28d46fdf0298e0956381cf6fac5d83d
Carlos Llamas <cmllamas@google.com>
2026-05-15 16:39:25 +0000

    bcmdhd: update cfg80211_op->remain_on_channel params
    Staging commit 4dbd1829045e ("wifi: cfg80211: Add MAC address filter to
    remain_on_channel") added a new 'rx_addr' parameter to the callback
    cfg80211_ops->remain_on_channel. Make the corresponding updates here.
    
    Link: https://git.kernel.org/wireless/wireless-next/c/4dbd1829045e
    Change-Id: I16b2faee5c4130a3f2219e7cc2d1ad6bb5e0a55e
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- wl_cfgscan.c
+++ wl_cfgscan.c
@@ -5373,13 +5373,14 @@ wl_priortize_scan_over_listen(struct bcm_cfg80211 *cfg,
 #if defined(WL_CFG80211_P2P_DEV_IF)
 s32
 wl_cfgscan_remain_on_channel(struct wiphy *wiphy, bcm_struct_cfgdev *cfgdev,
-	struct ieee80211_channel *channel, unsigned int duration, u64 *cookie)
+	struct ieee80211_channel *channel, unsigned int duration, u64 *cookie,
+	const u8 *rx_addr)
 #else
 s32
 wl_cfgscan_remain_on_channel(struct wiphy *wiphy, bcm_struct_cfgdev *cfgdev,
 	struct ieee80211_channel *channel,
 	enum nl80211_channel_type channel_type,
-	unsigned int duration, u64 *cookie)
+	unsigned int duration, u64 *cookie, const u8 *rx_addr)
 #endif /* WL_CFG80211_P2P_DEV_IF */
 {
 	s32 target_channel;
