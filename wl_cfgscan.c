b8db24b98b19428cc9851af75a4d1ec1aa37f483
Carlos Llamas <cmllamas@google.com>
2026-07-28 16:59:32 +0000

    bcmdhd4389: add rx_addr parameter to .remain_on_channel
    Upstream commit 4dbd1829045e ("wifi: cfg80211: Add MAC address filter to
    remain_on_channel") added an optional 'rx_addr' parameter to the
    ops->remain_on_channel callback in cfg80211_ops. Make the corresponding
    updates here.
    
    Link: https://git.kernel.org/torvalds/c/4dbd1829045e
    Change-Id: I6b7316983638db5ea87513de6ae4b87b52f78c91
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- wl_cfgscan.c
+++ wl_cfgscan.c
@@ -5373,7 +5373,8 @@ wl_priortize_scan_over_listen(struct bcm_cfg80211 *cfg,
 #if defined(WL_CFG80211_P2P_DEV_IF)
 s32
 wl_cfgscan_remain_on_channel(struct wiphy *wiphy, bcm_struct_cfgdev *cfgdev,
-	struct ieee80211_channel *channel, unsigned int duration, u64 *cookie)
+	struct ieee80211_channel *channel, unsigned int duration, u64 *cookie,
+	const u8 *rx_addr)
 #else
 s32
 wl_cfgscan_remain_on_channel(struct wiphy *wiphy, bcm_struct_cfgdev *cfgdev,
